3dbd838e1429a3b6d2c6f2534c828a48581adbeb
Vommy <81606492+Vommy@users.noreply.github.com>
2021-04-13 18:35:00 +1200

    Fixed the decimal to hexadecimal number converter
    Made it so that the decimal to hexadecimal converter would:
    * Split the binary number into nibbles (4 bits)
    * Add 0's to each nibble in the binarynumber list that was less than the length of an actual nibble (4)
    * Assign the binary number list to a new list variable called hexa_decimal_num
    * Reverse each list inside the hexa_decimal_num list
    * Multiplies each element inside each list inside the hexa_decimal_num list by a positional list ( [8 ,4, 2, 1])
    * Prints out the hexa_decimal_num list

--- binary.py
+++ binary.py
@@ -13,28 +13,28 @@ def menu():
        Press 6 to convert hexadecimal to binary. \n
        Press E to exit \n
        """)
-    userChoice = input()
-    if userChoice.isnumeric():
-        if int(userChoice) == 1:
-            userInputNumber = input("What number would you like to convert? \n")
-            binaryconverter(userInputNumber)
-        elif int(userChoice) == 2:
-            userInputNumber = input("What number would you like to convert? \n")
-            decimalToHexadecimal(userInputNumber)
+    user_choice = input()
+    if user_choice.isnumeric():
+        if int(user_choice) == 1:
+            user_input_number = input("What number would you like to convert? \n")
+            binaryconverter(user_input_number)
+        elif int(user_choice) == 2:
+            user_input_number = input("What number would you like to convert? \n")
+            decimal_to_hexadecimal(user_input_number)
             # Run decimal to hexadecimal converter
-        elif int(userChoice) == 3:
+        elif int(user_choice) == 3:
             print("Still working on it")
             menu()
             # Run binary to decimal converter
-        elif int(userChoice) == 4:
+        elif int(user_choice) == 4:
             print("Still working on it")
             menu()
             # Run binary to hexadecimal
-        elif int(userChoice) == 5:
+        elif int(user_choice) == 5:
             print("Still working on it")
             menu()
             # Run hexadecimal to decimal
-        elif int(userChoice) == 6:
+        elif int(user_choice) == 6:
             print("Still working on it")
             menu()
             # Run hexadecimal to binary
@@ -42,7 +42,7 @@ def menu():
             print("Please choose an option using the assigned keys.")
             menu()
 
-    elif userChoice == "e" or userChoice == "E":
+    elif user_choice == "e" or user_choice == "E":
         print("Thank you for using the number converter")
         exit()
 
@@ -50,7 +50,7 @@ def menu():
         print("Please choose an option using the assigned keys.")
         menu()
 
-def binaryconverter(userInputNumber):
+def binaryconverter(user_input_number):
     """
     This functions takes a user input number and converts it into it's equivalent binary number.
     FORMAT: binaryconverter(x)
@@ -58,29 +58,29 @@ def binaryconverter(userInputNumber):
     :return: binary equivalent
     """
     import math                                                                     #Importing Math module
-    binaryNum = []                                                                  #Creates a list variable named binarynum
-    if userInputNumber.isnumeric:
-        userInputNumber = int(userInputNumber)
-        resofDiv2 = userInputNumber                                                     #Holding the user's input number in a temporary variable
-        resOfDiv = math.floor(userInputNumber/2)                                        #Divides the user's input number by 2 and then floors the result
-        remofDiv= resofDiv2 % 2                                                         #Modulus operator used on the user's input number. Result is either 1 or 0
-        binaryNum.append(remofDiv)                                                      #Appends the modulus operation result to the end of binarynum.
-        while  resOfDiv >= 1:                                                           #Creates a While loop that will run until the variable "resOfDiv" is less than 1.
-            resofDiv2 =resOfDiv
-            resOfDiv = math.floor(resofDiv2/2)
-            remofDiv = resofDiv2 % 2
-            binaryNum.append(remofDiv)
+    binary_num = []                                                                  #Creates a list variable named binary_num
+    if user_input_number.isnumeric:
+        user_input_number = int(user_input_number)
+        res_of_div_2 = user_input_number                                                     #Holding the user's input number in a temporary variable
+        res_of_div = math.floor(user_input_number/2)                                        #Divides the user's input number by 2 and then floors the result
+        rem_of_div = res_of_div_2 % 2                                                         #Modulus operator used on the user's input number. Result is either 1 or 0
+        binary_num.append(rem_of_div)                                                      #Appends the modulus operation result to the end of binary_num.
+        while res_of_div >= 1:                                                           #Creates a While loop that will run until the variable "res_of_div" is less than 1.
+            res_of_div_2 = res_of_div
+            res_of_div = math.floor(res_of_div_2/2)
+            rem_of_div = res_of_div_2 % 2
+            binary_num.append(rem_of_div)
         else:                                                                           #Once the while loop finishes, print out the result.
-            binaryNum.reverse()
-            print("Answer = {} " .format(binaryNum), sep="")
+            binary_num.reverse()
+            print("Answer = {} " .format(binary_num), sep="")
             print("")
             def exitLoop():
                     answer = \
                         input("Would you like to convert another number to binary? (Y/N) \n")
                     if answer == "Y" or answer == "y":
-                        userInputNumber = \
+                        user_input_number = \
                         input("What number would you like to convert? \n")
-                        binaryconverter(userInputNumber)
+                        binaryconverter(user_input_number)
                     elif answer == "N" or answer == "n":
                         answer = input("Would you like to return to the menu? (Y/N) \n")
                         if answer == "Y" or answer == "y":
@@ -98,35 +98,56 @@ def binaryconverter(userInputNumber):
         binaryconverter(x)
 
 
-def binaryBreaker(lengthofbinarynum):
-    for i in range(0, len(lengthofbinarynum), 4):
-        yield lengthofbinarynum[i:i + 4]
+def binaryBreaker(binary_num):
+    for i in range(0, len(binary_num), 4):
+        yield binary_num[i:i + 4]
 
-def decimalToHexadecimal(userInputNumber):
+def decimal_to_hexadecimal(user_input_number):
     import math  # Importing Math module
-    binaryNum = []  # Creates a list variable named binarynum
-    if userInputNumber.isnumeric:
-        userInputNumber = int(userInputNumber)
-        resofDiv2 = userInputNumber  # Holding the user's input number in a temporary variable
-        resOfDiv = math.floor(userInputNumber / 2)  # Divides the user's input number by 2 and then floors the result
-        remofDiv = resofDiv2 % 2  # Modulus operator used on the user's input number. Result is either 1 or 0
-        binaryNum.append(remofDiv)  # Appends the modulus operation result to the end of binarynum.
-        while resOfDiv >= 1:  # Creates a While loop that will run until the variable "resOfDiv" is less than 1.
-            resofDiv2 = resOfDiv
-            resOfDiv = math.floor(resofDiv2 / 2)
-            remofDiv = resofDiv2 % 2
-            binaryNum.append(remofDiv)
+    nibble_length = [0, 0, 0, 0]
+    positional_list = [8, 4, 2, 1]
+
+    binary_num = []  # Creates a list variable named binary_num
+    if user_input_number.isnumeric:
+        user_input_number = int(user_input_number)
+        res_of_div_2 = user_input_number  # Holding the user's input number in a temporary variable
+        res_of_div = math.floor(user_input_number / 2)  # Divides the user's input number by 2 and then floors the result
+        rem_of_div = res_of_div_2 % 2  # Modulus operator used on the user's input number. Result is either 1 or 0
+        binary_num.append(rem_of_div)  # Appends the modulus operation result to the end of binary_num.
+        while res_of_div >= 1:  # Creates a While loop that will run until the variable "res_of_div" is less than 1.
+            res_of_div_2 = res_of_div
+            res_of_div = math.floor(res_of_div_2 / 2)
+            rem_of_div = res_of_div_2 % 2
+            binary_num.append(rem_of_div)
         else:  # Once the while loop finishes, separate the binary number list into lists of 4, then for each group of 4 bits, mathematically operate on each list, and then assign the result of the operation to the hexadecimal list.
-            hexadecimalNum = list(binaryBreaker(binaryNum)) #Separates the binary number into groups of 4.
+            hexadecimalNum = list(binaryBreaker(binary_num)) #Separates the binary number into groups of 4.
+            for i in range(0, len(hexadecimalNum), 1):
+                temporarylist = hexadecimalNum[i]
+                if len(hexadecimalNum[i]) < len(nibble_length):
+                    while len(temporarylist) < len(nibble_length):
+                        temporarylist.append(0)
+                    hexadecimalNum[i] = temporarylist
+            hexadecimalNum.reverse() #Reversing
+            for i in range(0, len(hexadecimalNum), 1):
+                temp_list = (hexadecimalNum[i])
+                temp_list.reverse()
+                hexadecimalNum[i] = list(temp_list)
+            for i in range(0, len(hexadecimalNum), 1):
+                empty_list =[]
+                list_to_multiply = hexadecimalNum[i]
+                for num1, num2 in zip(list_to_multiply, positional_list):
+                    empty_list.append(num1 * num2)
+                hexadecimalNum[i] = empty_list
+            print(hexadecimalNum)
 
 
             def exitLoop():
                 answer = \
-                    input("Would you like to convert another number to binary? (Y/N) \n")
+                    input("Would you like to convert another number to hexadecimal? (Y/N) \n")
                 if answer == "Y" or answer == "y":
-                    userInputNumber = \
+                    user_input_number = \
                         input("What number would you like to convert? \n")
-                    binaryconverter(userInputNumber)
+                    decimal_to_hexadecimal(user_input_number)
                 elif answer == "N" or answer == "n":
                     answer = input("Would you like to return to the menu? (Y/N) \n")
                     if answer == "Y" or answer == "y":
@@ -147,7 +168,6 @@ def converterPage():
     Runs the introduction to the converter program
     :return:
     """
-    userChoice = 0
     import time
     print("Welcome to the number converter program! \n")
     time.sleep(2)
